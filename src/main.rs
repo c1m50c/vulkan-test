2fef94bfd9cfcf15a8e1ea6764816d4303c984c2
c1m50c <terminalityms@gmail.com>
2022-03-05 16:46:32 -0600

    Barebones Window Rendering 💨🍞

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use winit::event_loop::{ControlFlow, EventLoop};
+use winit::event::{Event, WindowEvent};
+use winit::window::WindowBuilder;
+
+
 fn main() {
-    println!("Hello, world!");
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new()
+        .build(&event_loop)
+        .unwrap();
+    
+    event_loop.run(move |event, _, control_flow| {
+        match event {
+            Event::WindowEvent { event: WindowEvent::CloseRequested, .. } => {
+                *control_flow = ControlFlow::Exit;
+            },
+
+            Event::MainEventsCleared => { window.request_redraw(); },
+
+            Event::RedrawRequested(id) => {
+                // Rendering
+            },
+
+            e => {  },
+        }
+    });
 }
